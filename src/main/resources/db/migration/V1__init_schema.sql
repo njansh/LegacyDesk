c45d33a685e9def9360955d30099d1c7039ffead
Njansh <nadsonjhonyobmep@gmail.com>
2026-08-17 19:44:49 -0300

    feat: configure flyway database migrations (#5)

---
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,9 @@
+﻿-- Baseline system initialization migration
+CREATE TABLE system_metadata (
+                                 id VARCHAR(50) NOT NULL PRIMARY KEY,
+                                 system_version VARCHAR(20) NOT NULL,
+                                 initialized_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+INSERT INTO system_metadata (id, system_version)
+VALUES ('SYSTEM_INIT', '1.0.0');
