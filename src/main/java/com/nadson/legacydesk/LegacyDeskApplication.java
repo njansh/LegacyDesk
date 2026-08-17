3f69e26930eb1d5f02cd8a50d56796665c592992
Njansh <nadsonjhonyobmep@gmail.com>
2026-08-17 19:08:55 -0300

    chore: setup legacy Spring Boot 2.7 and Java 8 baseline (#3)

---
+++ src/main/java/com/nadson/legacydesk/LegacyDeskApplication.java
@@ -0,0 +1,12 @@
+package com.nadson.legacydesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegacyDeskApplication.class, args);
+    }
+}
---
+++ src/test/java/com/nadson/legacydesk/LegacyDeskApplicationTests.java
@@ -0,0 +1,12 @@
+package com.nadson.legacydesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
